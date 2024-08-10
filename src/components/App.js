64762c27b57db182f0b1b4a8503d9668262baf3b
musin2 <muhsindaud75@gmail.com>
2024-08-10 18:42:16 +0300

    adds useEffect that fetches data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
+import { useEffect, useState } from "react";
+
 // create your App component here
+function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res)=> res.json())
+      .then((data)=>{setFetchedData((x) => x=data.message)
+        setIsLoaded(true)
+      })
+  },[])
+
+
+  if (!isLoaded) {
+    return <p>"Loading..."</p>;
+  } 
+
+  return <img src={fetchedData} alt="A Random Dog"></img>;
+}
+
+export default App;
